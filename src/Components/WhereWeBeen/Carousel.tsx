a3171ee2b618a86cd37298f79eb9c347b3c67bc4
Hieu <hieun201104@gmail.com>
2025-04-13 22:11:51 -0700

    Feature: Carousel dark mode suppport

--- src/Components/WhereWeBeen/Carousel.tsx
+++ src/Components/WhereWeBeen/Carousel.tsx
@@ -1,67 +1,83 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import useEmblaCarousel from "embla-carousel-react";
 import AutoScroll from "embla-carousel-auto-scroll";
 import CarouselCard from "./CarouselCard";
 
 // 1) Manually import each SVG:
-import amazon from "../../Assets/Images/homepage/company logos/amazon.svg";
-import amazonsmall from "../../Assets/Images/homepage/company logos/amazonsmall.svg";
-import amex from "../../Assets/Images/homepage/company logos/amex.svg";
-import atnt from "../../Assets/Images/homepage/company logos/atnt.svg";
-import attIcon from "../../Assets/Images/homepage/company logos/att-icon.svg";
-import aurora from "../../Assets/Images/homepage/company logos/aurora.svg";
-import awsSmall from "../../Assets/Images/homepage/company logos/aws-small.svg";
-import aws from "../../Assets/Images/homepage/company logos/aws.svg";
-import blackrock from "../../Assets/Images/homepage/company logos/blackrock.svg";
-import cisco from "../../Assets/Images/homepage/company logos/cisco.svg";
-import citi from "../../Assets/Images/homepage/company logos/citi.svg";
-import deloitte from "../../Assets/Images/homepage/company logos/deloitte.svg";
-import facebookSmall from "../../Assets/Images/homepage/company logos/facebook-small.svg";
-import facebook from "../../Assets/Images/homepage/company logos/facebook.svg";
-import fedexFreight from "../../Assets/Images/homepage/company logos/fedex-freight.svg";
-import ficoLogo from "../../Assets/Images/homepage/company logos/FICO_logo.svg";
-import goldmanSachs from "../../Assets/Images/homepage/company logos/goldman-sachs.svg";
-import googleSmall from "../../Assets/Images/homepage/company logos/google-small.svg";
-import google from "../../Assets/Images/homepage/company logos/google.svg";
-import intel from "../../Assets/Images/homepage/company logos/intel.svg";
-
-// 2) Put them in a single array:
-const companyLogos = [
-  amazon,
-  amex,
-  atnt,
-  attIcon,
-  aurora,
-  aws,
-  blackrock,
-  cisco,
-  citi,
-  deloitte,
-  facebookSmall,
-  facebook,
-  fedexFreight,
-  ficoLogo,
-  goldmanSachs,
-  google,
-  intel
-];
+import amazon from "../../Assets/Images/homepage/company logos/pngs/amazon.png";
+import amazon_dark from "../../Assets/Images/homepage/company logos/pngs/amazon_darkmode.png";
+import amex from "../../Assets/Images/homepage/company logos/pngs/amex.png";
+import atnt from "../../Assets/Images/homepage/company logos/pngs/atnt.png";
+import atnt_dark from "../../Assets/Images/homepage/company logos/pngs/atnt_darkmode.png";
+import aurora from "../../Assets/Images/homepage/company logos/pngs/aurora.png";
+import aurora_dark from "../../Assets/Images/homepage/company logos/pngs/aurora_darkmode.png";
+import aws from "../../Assets/Images/homepage/company logos/pngs/aws.png";
+import aws_dark from "../../Assets/Images/homepage/company logos/pngs/aws_darkmode.png";
+import blackrock from "../../Assets/Images/homepage/company logos/pngs/blackrock.png";
+import blackrock_dark from "../../Assets/Images/homepage/company logos/pngs/blackrock_darkmode.png";
+import cisco from "../../Assets/Images/homepage/company logos/pngs/cisco.png";
+import citi from "../../Assets/Images/homepage/company logos/pngs/citi.png";
+import deloitte from "../../Assets/Images/homepage/company logos/pngs/deloitte.png";
+import deloitte_dark from "../../Assets/Images/homepage/company logos/pngs/deloitte_darkmode.png";
+import facebook from "../../Assets/Images/homepage/company logos/pngs/facebook.png";
+import fedexFreight from "../../Assets/Images/homepage/company logos/pngs/fedex-freight.png";
+import ficoLogo from "../../Assets/Images/homepage/company logos/pngs/FICO_logo.png";
+import goldmanSachs from "../../Assets/Images/homepage/company logos/pngs/goldman-sachs.png";
+import google from "../../Assets/Images/homepage/company logos/pngs/google.png";
+import intel from "../../Assets/Images/homepage/company logos/pngs/intel.png";
+import { ThemeContext } from "../../Pages/Home/ThemeContext";
 
 export default function Carousel() {
   // Adjust the speed to your preference
   const [speed] = useState(2);
+  const { isDark } = useContext(ThemeContext);
+  // logos in one array with dark mode :
+  const companyLogos = isDark
+    ? [
+        amazon_dark,
+        amex,
+        atnt_dark,
+        aurora_dark,
+        aws_dark,
+        blackrock_dark,
+        cisco,
+        citi,
+        deloitte_dark,
+        facebook,
+        fedexFreight,
+        ficoLogo,
+        goldmanSachs,
+        google,
+        intel
+      ]
+    : [
+        amazon,
+        amex,
+        atnt,
+        aurora,
+        aws,
+        blackrock,
+        cisco,
+        citi,
+        deloitte,
+        // facebookSmall,
+        facebook,
+        fedexFreight,
+        ficoLogo,
+        goldmanSachs,
+        google,
+        intel
+      ];
 
   // Set up Embla with autoScroll plugin:
-  const [emblaRef, emblaApi] = useEmblaCarousel(
-    { loop: true },
-    [
-      AutoScroll({
-        playOnInit: true,
-        speed,
-        stopOnInteraction: false,
-        stopOnFocusIn: false
-      })
-    ]
-  );
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true }, [
+    AutoScroll({
+      playOnInit: true,
+      speed,
+      stopOnInteraction: false,
+      stopOnFocusIn: false
+    })
+  ]);
 
   useEffect(() => {
     if (emblaApi) {
--- src/Components/WhereWeBeen/CarouselCard.tsx
+++ src/Components/WhereWeBeen/CarouselCard.tsx
@@ -4,7 +4,7 @@ interface CarouselCardProps {
 
 export default function CarouselCard({ src }: CarouselCardProps) {
   return (
-    <div className="embla__slide w-[12vw] h-[10vw] flex-shrink-0 mx-[3vw] border-2 border-white rounded-lg bg-white flex justify-center items-center">
+    <div className="embla__slide max-w-[clamp(200px,20vw,500px)] h-[clamp(50px,7vw,200px)] flex-shrink-0 mx-[1.3vw]  rounded-lg  flex justify-center items-center">
       <img src={src} alt="logo" className="object-contain w-full h-full" />
     </div>
   );
